381b02316e988e3b0447a4d12d0f55b69fbefa92
Shamarni McFarlane <Shamarni.mcfarlane@ft.com>
2024-02-23 16:32:32 +0000

    update toolkit dependancies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-	extends: ['@financial-times/eslint-config-next'],
-	rules: {
-		indent: 'off',
-	  },
-	env: {
-		es6: true,
-		node: true
-	}
+	extends: ['@financial-times/eslint-config-next']
 };
